6c3725c23ac153b9311ca9010aecbf6a3dad40de
Troye Gilbert <gilberttroye@gmail.com>
2020-07-12 15:14:33 +0300

    12th Uly changes

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\troye\Documents\PersonalProjects\DjangoProjects\darajaAPI\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
