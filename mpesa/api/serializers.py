b8c9b9f088683402789ecc357f399974bf6eab98
troyegilbert <gilberttroye@gmail.com>
2020-08-02 19:23:20 +0300

    updated serializer

--- mpesa/api/serializers.py
+++ mpesa/api/serializers.py
@@ -5,4 +5,4 @@ from mpesa.models import LNMOnline
 class LNMOnlineSerializer(serializers.ModelSerializer):
     class Meta:
         model = LNMOnline
-        fields = ['id', 'account_name', 'users', 'created']
+        fields = ['id']
