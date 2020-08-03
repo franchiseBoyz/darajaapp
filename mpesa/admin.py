1ad6659b1e5366d2fe4634cf098d2087c3e6b598
troyegilbert <gilberttroye@gmail.com>
2020-08-04 00:11:15 +0300

    model save

--- mpesa/admin.py
+++ mpesa/admin.py
@@ -1,16 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from mpesa.models import LNMOnline, C2BPayments
+from mpesa.models import LNMOnline
 
 
 class LNMOnlineAdmin(admin.ModelAdmin):
     list_display = ("PhoneNumber", "Amount", "MpesaReceiptNumber", "TransactionDate")
 
 admin.site.register(LNMOnline,LNMOnlineAdmin)
-
-
-class C2BPaymentsAdmin(admin.ModelAdmin):
-    list_display = ("MSISDN", "TransAmount", "TransID", "TransTime")
-
-admin.site.register(C2BPayments,C2BPaymentsAdmin)
--- mpesa/api/views.py
+++ mpesa/api/views.py
@@ -15,13 +15,32 @@ class LNMCallbackUrlAPIView(CreateAPIView):
         print (request.data,"This is request.data")
 
 
+        merchant_request_id = request.data["Body"]["stkCallback"]["MerchantRequestID"]
+        checkout_request_id = request.data["Body"]["stkCallback"]["CheckoutRequestID"]
+        result_code = request.data["Body"]["stkCallback"]["ResultCode"]
+        result_description = request.data["Body"]["stkCallback"]["ResultDesc"]
+        amount = request.data["Body"]["stkCallback"]["CallbackMetadata"]['Item'][0]['Value']
+        mpesa_receipt_number = request.data["Body"]["stkCallback"]["CallbackMetadata"]['Item'][1]['Value']
+        balance = ""
+        transaction_date = request.data["Body"]["stkCallback"]["CallbackMetadata"]['Item'][3]['Value']
+        phone_number = request.data["Body"]["stkCallback"]["CallbackMetadata"]['Item'][4]['Value']
 
-merchant_request_id = request.data["Body"]["stkCallback"]["MerchantRequestID"]
-checkout_request_id = request.data["Body"]["stkCallback"]["CheckoutRequestID"]
-result_code = request.data["Body"]["stkCallback"]["ResultCode"]
-result_description = request.data["Body"]["stkCallback"]["ResultDesc"]
-amount = request.data["Body"]["stkCallback"]["CallbackMetadata"]['Item'][0]['Value']
-mpesa_receipt_number = request.data["Body"]["stkCallback"]["CallbackMetadata"]['Item'][1]['Value']
-balance = ""
-transaction_date = request.data["Body"]["stkCallback"]["CallbackMetadata"]['Item'][3]['Value']
-phone_number = request.data["Body"]["stkCallback"]["CallbackMetadata"]['Item'][4]['Value']
+        from datetime import datetime
+
+        str_transaction_date = str(transaction_date)
+        transaction_datetime = datetime.strptime(str_transaction_date, "%Y%m%d%H%M%S")
+
+        from mpesa.models import LNMOnline
+
+        our_model = LNMOnline.objects.create(
+            CheckoutRequestID = checkout_request_id,
+            MerchantRequestID = merchant_request_id,
+            ResultCode = result_code,
+            ResultDesc = result_description,
+            MpesaReceiptNumber = mpesa_receipt_number,
+            Balance = balance,
+            TransactionDate = transaction_datetime,
+            PhoneNumber = phone_number,
+        )
+
+        our_model.save()
