f582af3b3c7d53060466f1d6fca1e58660b319ed
troyegilbert <gilberttroye@gmail.com>
2020-08-03 22:28:56 +0300

    updated views.py

--- mpesa/api/views.py
+++ mpesa/api/views.py
@@ -13,3 +13,13 @@ class LNMCallbackUrlAPIView(CreateAPIView):
 
     def create(self, request):
         print (request.data,"This is request.data")
+
+merchant_request_id = request.data["Body"]["stkCallback"]["MerchantRequestID"]
+checkout_request_id = request.data["Body"]["stkCallback"]["CheckoutRequestID"]
+result_code = request.data["Body"]["stkCallback"]["ResultCode"]
+result_description = request.data["Body"]["stkCallback"]["ResultDesc"]
+amount = request.data["Body"]["stkCallback"]["CallbackMetadata"]['Item'][0]['Value']
+mpesa_receipt_number = request.data["Body"]["stkCallback"]["CallbackMetadata"]['Item'][1]['Value']
+balance = ""
+transaction_date = request.data["Body"]["stkCallback"]["CallbackMetadata"]['Item'][3]['Value']
+phone_number = request.data["Body"]["stkCallback"]["CallbackMetadata"]['Item'][4]['Value']
